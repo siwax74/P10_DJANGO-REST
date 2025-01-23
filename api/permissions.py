721682b8edc65f18c9fd17aa3b9744ca1a491101
Siwax74 <damien.gey74@gmail.com>
2025-01-23 19:32:30 +0100

    23_01_2025_12:30 - Project_75%

--- api/permissions.py
+++ api/permissions.py
@@ -2,12 +2,11 @@ from rest_framework.permissions import BasePermission
 from api.models.contributor import Contributor
 
 
-def check_contributor(user, project):
-    for contributor in Contributor.objects.filter(project_id=project.id):
+def check_contributor(user, project_id):
+    for contributor in Contributor.objects.filter(project_id=project_id):
         if user == contributor.user_id:
             return True
     return False
-
 
 class ContributorViewsetPermission(BasePermission):
     """
@@ -41,33 +40,35 @@ class ProjectPermission(BasePermission):
     message = "You don't have permissions to do that."
 
     def has_permission(self, request, view):
-        print(f"Checking global has_permission for user: {request.user}")
-        return request.user and request.user.is_authenticated
+        if view.action in ["create"]:
+            return request.user and request.user.is_authenticated
+        return True
 
     def has_object_permission(self, request, view, obj):
         if view.action in ["retrieve", "list"]:
             is_contributor = check_contributor(request.user.id, obj)
-            print(f"Checking contributor retrieve/list permission for user {request.user}: {is_contributor}")
             return is_contributor
         elif view.action in ["update", "partial_update", "destroy"]:
             is_author = request.user == obj.author
-            print(f"Checking author permission for user {request.user}: {is_author}")
             return is_author
-
+        return True
 
 class IssuePermission(BasePermission):
     """
     Issue author can Update and Delete their issues.
     Project contributors can List all project issues, Read issue or Create issue.
     """
+    message = "You don't have permissions to do that."
 
-    message = "You dont have permission to do that."
+    def has_permission(self, request, view):
+        if view.action in ['list', 'create', 'retrieve']:
+            # Lister / crée / détails les issues nécessite que l'utilisateur soit un contributeur du projet
+            project_id = view.kwargs.get('projects_pk')
+            return check_contributor(request.user, project_id)
+        return True
 
     def has_object_permission(self, request, view, obj):
-        if not request.user.is_authenticated:
-            return False
-
-        if view.action in ["retrieve", "list", "create"]:
-            return check_contributor(request.user, obj.project_id)
-        elif view.action in ["update", "partial_update", "destroy"]:
+        if view.action in ['update', 'partial_update', 'destroy']:
+            # Seul l'auteur de l'issue peut la modifier ou la supprimer
             return request.user == obj.author
+        return True
--- api/serializers/contributor_serializer.py
+++ api/serializers/contributor_serializer.py
@@ -3,8 +3,6 @@ from api.models.contributor import Contributor
 
 
 class ContributorSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()  # Afficher le nom d'utilisateur
-
     class Meta:
         model = Contributor
         fields = ["id", "user", "project"]
